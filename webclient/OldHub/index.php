e949df730608b18bd24d2df6265df12051822ac2
A bad coder <michaelkoessl1811@gmail.com>
2017-01-14 00:13:56 +0100

    ayayayay

---
+++ webclient/OldHub/index.php
@@ -0,0 +1,32 @@
+<html lang='en'>
+    <head>
+        <title>Project B - Webclient</title>
+    </head>
+	<style>
+	div {
+	   width: 800px; 
+	   height: 20px;
+	   background: url(http://bledixon.com/images/client_bottom.jpg);
+       box-shadow: 0px 5px 1px #888888;
+       }
+	</style>
+    <body background="http://bledixon.com/images/bgwebclient.jpg">
+	<center>
+	   <img src="http://bledixon.com/images/header.jpg" alt="" />
+	</center>
+	   <center>
+            <script src='http://www.realmofthemadgod.com/js/rotmg.UrlLib.js'></script>
+            <embed src='http://bledixon.github.io/client/WebMain.swf' width='800' height='600'>
+	   </center>
+	     <center>
+		 
+	       <div>
+		   <a color="black" href="http://bledixon.com/webclient/">Refresh!</a>
+		   </div>
+	     </center>
+	   <center>
+	   <font size="5" color="black">If you want to play on Flash Projector</font>
+	   <p color="white">http://bledixon.github.io/client/WebMain.swf</p>
+	   </center>
+    </body>
+</html>
